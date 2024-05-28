42b65fc82b3366b53a24dc5c653db3a39a81179e
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-28 23:18:11 +0300

    Create search bar common component

---
+++ src/components/common/searchbar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import searchIcon from "../../assets/icons/search-icon.svg";
+
+const searchValue = defineModel("search-value", {
+  type: String,
+});
+
+defineProps({
+  placeholderTitle: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <div
+    class="flex w-full items-center gap-0.5 rounded-full bg-inputBg px-3 py-2"
+  >
+    <img class="mr-2" :src="searchIcon" />
+    <input
+      v-model="searchValue"
+      type="text"
+      :placeholder="`Search by ${placeholderTitle}` || 'Search...'"
+      class="border-none bg-transparent outline-none"
+    />
+  </div>
+</template>
