779fa37b066c033d6954643330ee317984fdcc51
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-28 22:02:10 +0300

    Add Breadcrumb

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import Header from "./components/Header.vue";
 import Navbar from "./components/Navbar.vue";
+import Breadcrumb from "./components/Breadcrumb.vue";
 </script>
 
 <template>
   <div class="flex flex-col">
     <Header />
     <Navbar />
+    <Breadcrumb />
   </div>
 </template>
---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex gap-2 border-t py-5 px-12 text-[13px]">
+    <button class="">Home</button>
+    <span class="text-subtitle">/</span>
+    <button class="">Furniture</button>
+    <span class="text-subtitle">/</span>
+    <button class="text-subtitle">Dining Room Tables</button>
+  </div>
+</template>
