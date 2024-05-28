18a63d6f03a676752205e2210a30d49a8f55ee47
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-29 00:30:06 +0300

    Displayed brands filter in sidebar

--- src/components/sidebar/brandFilter.vue
+++ src/components/sidebar/brandFilter.vue
@@ -1,5 +1,22 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import useBrands from "../../composables/useBrands.ts";
+import filterList from "../common/filterList.vue";
+
+const { brands, fetchBrands } = useBrands();
+
+const selectedFilters = ref<string[]>([]);
+
+await fetchBrands();
+
+console.log(brands.value);
+</script>
+
 <template>
-  <div>
-    <p>Brand Filter</p>
-  </div>
+  <filterList
+    v-if="brands"
+    title="Brand"
+    v-model:selectedFilters="selectedFilters"
+    :list="brands.data"
+  />
 </template>
