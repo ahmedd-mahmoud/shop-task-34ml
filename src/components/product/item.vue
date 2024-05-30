b5837a3c5ac99d219786362240746f4753a09c75
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-30 03:05:46 +0300

    Add wishlist icon to each product item

--- src/components/product/item.vue
+++ src/components/product/item.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { PropType } from "vue";
 import { ProductResponse } from "../../types/api";
+import wishlistIcon from "../../assets/icons/wishlist.svg";
 
 defineProps({
   brand: {
@@ -12,11 +13,16 @@ defineProps({
 
 <template>
   <div class="flex flex-col items-start">
-    <img
-      :src="brand.featured_image"
-      alt="brand.name"
-      class="w-full h-[296px] object-cover"
-    />
+    <div class="relative">
+      <img
+        :src="brand.featured_image"
+        alt="brand.name"
+        class="w-full h-[296px] object-cover"
+      />
+      <button class="absolute top-2 right-2 p-2 bg-white rounded-full">
+        <img :src="wishlistIcon" alt="Add to wishlist" />
+      </button>
+    </div>
     <p class="font-bold text-lg">{{ brand.title }}</p>
     <p>{{ brand.default_variant.price }}</p>
   </div>
