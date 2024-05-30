6a3ff7ac8e928adbc91a847b17dd5aa7393ff078
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-30 07:11:11 +0300

    Created common button component

---
+++ src/components/common/button.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { PropType } from "vue";
+
+defineProps({
+  text: {
+    type: String,
+  },
+  icon: {
+    type: String,
+  },
+  type: {
+    type: String as PropType<"solid" | "outline" | "icon">,
+    default: "solid",
+  },
+});
+
+const typeClass = {
+  solid: "bg-buttonBg text-white hover:bg-buttonBg/80",
+  outline: "text-buttonBg border border-buttonBg hover:bg-buttonBg/20",
+  icon: "hover:opacity-80",
+};
+</script>
+
+<template>
+  <button
+    class="flex items-center font-bold text-[16px] rounded-lg p-1"
+    :class="typeClass[type]"
+    @click="$emit('click')"
+  >
+    <span v-if="text">{{ text }}</span>
+    <img v-if="icon" :src="icon" alt="icon" :class="typeClass.icon" />
+  </button>
+</template>
