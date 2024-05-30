b36b4dcf393e7f9102e783ee66da443207663a61
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-31 00:37:47 +0300

    Add loading indicator

--- src/components/product/list.vue
+++ src/components/product/list.vue
@@ -6,22 +6,26 @@ import useBrands from "../../composables/useBrands.ts";
 import item from "./item.vue";
 import filterBar from "../filters/filterBar/index.vue";
 import pagination from "../common/pagination.vue";
+import loadingIcon from "../../assets/icons/loading-icon.svg";
 
 const { products, fetchProducts } = useProducts();
 const { selectedCategoryFilters } = useCategory();
 const { selectedBrandFilters } = useBrands();
 
 const currentPage = ref(1);
+const loading = ref(false);
 
 const filterItems = computed(() => {
   return [...selectedCategoryFilters.value, ...selectedBrandFilters.value];
 });
 
 watchEffect(async () => {
+  loading.value = true;
   if (filterItems.value.length > 0) {
     currentPage.value = 1;
   }
   await fetchProducts(filterItems.value, currentPage.value);
+  loading.value = false;
 });
 </script>
 
@@ -29,10 +33,13 @@ watchEffect(async () => {
   <div v-if="products" class="flex grow flex-col gap-4">
     <filterBar />
 
-    <div class="flex justify-center lg:justify-normal flex-wrap gap-5">
+    <div
+      v-if="!loading"
+      class="flex justify-center lg:justify-normal flex-wrap gap-5"
+    >
       <item v-for="brand in products.data" :key="brand.id" :brand="brand" />
     </div>
-
+    <img v-else :src="loadingIcon" alt="Loading" class="w-56 self-center" />
     <pagination v-model:page="currentPage" :listInfo="products.pagination" />
   </div>
 </template>
