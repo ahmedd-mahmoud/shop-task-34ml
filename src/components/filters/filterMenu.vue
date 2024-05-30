cf0a99ca17654fa4469c8d6e8e58c50f0108f70c
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-05-31 01:36:09 +0300

    Enhance filter menu

--- src/components/filters/filterMenu.vue
+++ src/components/filters/filterMenu.vue
@@ -25,26 +25,28 @@ const clearAllFilters = () => {
 <template>
   <div class="lg:hidden">
     <div
-      class="flex flex-col gap-4 fixed left-0 top-0 z-10 w-full h-full overflow-auto bg-white transition-transform duration-500"
+      class="flex flex-col justify-between gap-4 fixed left-0 top-0 z-10 w-full h-full overflow-auto bg-white transition-transform duration-500"
       :class="isMenuOpen ? 'translate-x-0' : '-translate-x-full'"
     >
-      <div
-        class="flex justify-between items-center py-4 border-b px-5 sm:px-10"
-      >
-        <h1 class="font-bold">Filter By</h1>
-        <commonButton
-          :icon="closeIcon"
-          type="link"
-          @click="isMenuOpen = false"
-        />
-      </div>
-      <div class="flex flex-col gap-3 px-5 sm:px-10">
-        <categoryFilter />
-        <brandFilter />
+      <div class="flex flex-col gap-4">
+        <div
+          class="flex justify-between items-center py-4 border-b px-5 sm:px-10"
+        >
+          <h1 class="font-bold">Filter By</h1>
+          <commonButton
+            :icon="closeIcon"
+            type="link"
+            @click="isMenuOpen = false"
+          />
+        </div>
+        <div class="flex flex-col gap-3 px-5 sm:px-10">
+          <categoryFilter />
+          <brandFilter />
+        </div>
       </div>
 
       <div
-        class="flex justify-between items-center font-bold text-[16px] px-5 sm:px-10"
+        class="flex justify-between items-center font-bold text-[16px] py-3 px-5 sm:px-10"
       >
         <commonButton
           text="Clear All"
